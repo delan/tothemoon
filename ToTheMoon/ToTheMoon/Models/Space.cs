b85be4c5e11ef9653588f59e25792d4bd5f6db31
Kye Russell <me@kyerussell.com>
2014-04-09 15:08:32 +0800

    nicer names for attributes

--- ToTheMoon/ToTheMoon/Models/Space.cs
+++ ToTheMoon/ToTheMoon/Models/Space.cs
@@ -1,12 +1,18 @@
 ﻿using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToTheMoon.Models
 {
     public class Space
     {
         public int ID { get; set; }
-        public string Title { get; set; }
+        
+        public string Name { get; set; }
+
+        [Display (Name="Total Space")]
         public int SpaceTotal { get; set; }
+
+        [Display (Name="Space Used")]
         public int SpaceUsed { get; set; }
         //public ApplicationUser PrincipleInvestigator { get; set; }
     }
