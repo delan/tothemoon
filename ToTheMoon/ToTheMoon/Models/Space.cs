11b753abf42e8de35418cf07c0a9dd362d10f530
Kye Russell <me@kyerussell.com>
2014-04-09 14:35:37 +0800

    created basic model for storage spaces.

---
+++ ToTheMoon/ToTheMoon/Models/Space.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace ToTheMoon.Models
+{
+    public class Space
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int SpaceTotal { get; set; }
+        public int SpaceUsed { get; set; }
+        public ApplicationUser PrincipleInvestigator { get; set; }
+    }
+
+    public class SpaceDbContext : DbContext
+    {
+        public DbSet<Space> Spaces { get; set; }
+    }
+}
