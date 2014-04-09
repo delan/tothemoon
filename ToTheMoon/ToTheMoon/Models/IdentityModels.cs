96618c8f61918dbfb33a0d2066c47af51d00331b
Kye Russell <me@kyerussell.com>
2014-04-10 01:27:34 +0800

    removed unused code

--- ToTheMoon/ToTheMoon/Models/IdentityModels.cs
+++ ToTheMoon/ToTheMoon/Models/IdentityModels.cs
@@ -1,10 +1,21 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+  
 
 namespace ToTheMoon.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [Display (Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display (Name = "Last Name")]
+        public string LastName { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
