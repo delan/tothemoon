2850b7e4ff032d53d7d16d8c22e591cc26094536
mercluke <mercluke@gmail.com>
2014-05-28 20:06:16 +0800

    changed some documentation

--- ToTheMoon/ToTheMoon/Controllers/NewSpaceRequestController.cs
+++ ToTheMoon/ToTheMoon/Controllers/NewSpaceRequestController.cs
@@ -24,19 +24,19 @@ namespace ToTheMoon.Controllers
         }
 
         // GET: /NewSpaceRequest/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            NewSpaceRequest newspacerequest = db.NewSpaceRequests.Find(id);
-            if (newspacerequest == null)
-            {
-                return HttpNotFound();
-            }
-            return View(newspacerequest);
-        }
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //       return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //   }
+        //    NewSpaceRequest newspacerequest = db.NewSpaceRequests.Find(id);
+        //    if (newspacerequest == null)
+        //   {
+        //        return HttpNotFound();
+        //    }
+        //    return View(newspacerequest);
+        //}
 
         // GET: /NewSpaceRequest/Create
         public ActionResult Create()
@@ -70,35 +70,35 @@ namespace ToTheMoon.Controllers
         }
 
         // GET: /NewSpaceRequest/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            NewSpaceRequest newspacerequest = db.NewSpaceRequests.Find(id);
-            if (newspacerequest == null)
-            {
-                return HttpNotFound();
-            }
-            return View(newspacerequest);
-        }
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    NewSpaceRequest newspacerequest = db.NewSpaceRequests.Find(id);
+        //    if (newspacerequest == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(newspacerequest);
+        //}
 
         // POST: /NewSpaceRequest/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="ID,name,brief,comment,timestamp")] NewSpaceRequest newspacerequest)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(newspacerequest).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(newspacerequest);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include="ID,name,brief,comment,timestamp")] NewSpaceRequest newspacerequest)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(newspacerequest).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(newspacerequest);
+        //}
 
         // GET: /NewSpaceRequest/Delete/5
         public ActionResult Delete(int? id)
