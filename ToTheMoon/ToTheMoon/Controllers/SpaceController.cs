05f9c516de66b50494a7be7ee87a2946c697c548
mercluke <mercluke@gmail.com>
2014-05-31 20:34:03 +0800

    Add Space Controller
    because we need one

---
+++ ToTheMoon/ToTheMoon/Controllers/SpaceController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ToTheMoon.Models;
+using ToTheMoon.DAL;
+
+namespace ToTheMoon.Controllers
+{
+    public class SpaceController : Controller
+    {
+        private ProjectContext db = new ProjectContext();
+
+        // GET: /Space/
+        public ActionResult Index()
+        {
+            return View(db.Spaces.ToList());
+        }
+
+        // GET: /Space/Details/5
+        public ActionResult Review(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Space space = db.Spaces.Find(id);
+            if (space == null)
+            {
+                return HttpNotFound();
+            }
+            return View(space);
+        }
+
+        // POST: /Space/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="key,Name,capacity,used,increase,PIKey")] Space space)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(space).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(space);
+        }
+
+        // POST: /Space/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Space space = db.Spaces.Find(id);
+            db.Spaces.Remove(space);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
